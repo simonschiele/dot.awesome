265b9a448284145b6def72adab7f4c09ceb5aa7a
Simon Schiele <simon.schiele@ypsilon.net>
2011-11-07 23:07:36 +0100

    fixed paths in default themes

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -32,15 +32,15 @@ theme.border_marked = "#91231c"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+theme.taglist_squares_sel   = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/taglist/squarew.png"
 
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.tasklist_floating_icon = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -50,45 +50,45 @@ theme.menu_width  = "100"
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
+theme.wallpaper_cmd = { "awsetbg " .. os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/background.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/fairhw.png"
+theme.layout_fairv = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/fairvw.png"
+theme.layout_floating  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/magnifierw.png"
+theme.layout_max = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/maxw.png"
+theme.layout_fullscreen = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft   = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/tileleftw.png"
+theme.layout_tile = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/tilew.png"
+theme.layout_tiletop = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/tiletopw.png"
+theme.layout_spiral  = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/spiralw.png"
+theme.layout_dwindle = os.getenv("HOME") .. "/.config/awesome/" .. "/themes/default/layouts/dwindlew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
--- themes/sky/theme.lua
+++ themes/sky/theme.lua
@@ -24,55 +24,55 @@ theme.border_focus  = "#729fcf"
 theme.border_marked = "#eeeeec"
 
 -- IMAGES
-theme.layout_fairh           = "/usr/share/awesome/themes/sky/layouts/fairh.png"
-theme.layout_fairv           = "/usr/share/awesome/themes/sky/layouts/fairv.png"
-theme.layout_floating        = "/usr/share/awesome/themes/sky/layouts/floating.png"
-theme.layout_magnifier       = "/usr/share/awesome/themes/sky/layouts/magnifier.png"
-theme.layout_max             = "/usr/share/awesome/themes/sky/layouts/max.png"
-theme.layout_fullscreen      = "/usr/share/awesome/themes/sky/layouts/fullscreen.png"
-theme.layout_tilebottom      = "/usr/share/awesome/themes/sky/layouts/tilebottom.png"
-theme.layout_tileleft        = "/usr/share/awesome/themes/sky/layouts/tileleft.png"
-theme.layout_tile            = "/usr/share/awesome/themes/sky/layouts/tile.png"
-theme.layout_tiletop         = "/usr/share/awesome/themes/sky/layouts/tiletop.png"
+theme.layout_fairh           = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/fairh.png"
+theme.layout_fairv           = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/fairv.png"
+theme.layout_floating        = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/floating.png"
+theme.layout_magnifier       = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/magnifier.png"
+theme.layout_max             = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/max.png"
+theme.layout_fullscreen      = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/fullscreen.png"
+theme.layout_tilebottom      = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/tilebottom.png"
+theme.layout_tileleft        = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/tileleft.png"
+theme.layout_tile            = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/tile.png"
+theme.layout_tiletop         = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/tiletop.png"
 
-theme.awesome_icon           = "/usr/share/awesome/themes/sky/awesome-icon.png"
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/sky/layouts/floating.png"
+theme.awesome_icon           = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/awesome-icon.png"
+theme.tasklist_floating_icon = os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/layouts/floating.png"
 
 -- from default for now...
-theme.menu_submenu_icon     = "/usr/share/awesome/themes/default/submenu.png"
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
+theme.menu_submenu_icon     = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/submenu.png"
+theme.taglist_squares_sel   = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/taglist/squarew.png"
 
 -- MISC
-theme.wallpaper_cmd         = { "awsetbg /usr/share/awesome/themes/sky/sky-background.png" }
+theme.wallpaper_cmd         = { "awsetbg " os.getenv("HOME") .. "/.config/awesome" .. "/themes/sky/sky-background.png" }
 theme.taglist_squares       = "true"
 theme.titlebar_close_button = "true"
 theme.menu_height           = "15"
 theme.menu_width            = "100"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = os.getenv("HOME") .. "/.config/awesome" .. "/themes/default/titlebar/maximized_focus_active.png"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
