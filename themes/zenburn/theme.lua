aaef607cf59a63caf9e678871db726ce20b0fca8
Simon Schiele <simon.schiele@ypsilon.net>
2011-11-08 01:25:42 +0100

    a few path fixes and removed broken 'sky'

--- themes/zenburn/theme.lua
+++ themes/zenburn/theme.lua
@@ -8,7 +8,7 @@
 
 -- {{{ Main
 theme = {}
-theme.wallpaper_cmd = { "awsetbg " .. os.getenv("HOME") .. ".config/awesome/themes/zenburn/zenburn-background.png" }
+theme.wallpaper_cmd = { "awsetbg " .. os.getenv("HOME") .. "/.config/awesome/themes/zenburn/zenburn-background.png" }
 -- }}}
 
 -- {{{ Styles
