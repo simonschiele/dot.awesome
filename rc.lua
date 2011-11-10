8871c9d65e9091f6a9da97ea61e59b27afa24c7f
Simon Schiele <simon.schiele@ypsilon.net>
2011-11-10 23:16:43 +0100

    cleaned a few keymappings

--- rc.lua
+++ rc.lua
@@ -362,14 +362,14 @@ globalkeys = awful.util.table.join(
 )
 
 clientkeys = awful.util.table.join(
-    awful.key({ modkey,    }, "f", function (c) c.fullscreen = not c.fullscreen end),
-    awful.key({ modkey,    }, "x", function (c) c:kill() end),
-    awful.key({ modkey, "Shift" }, "x", function () awful.util.spawn(xkill) end),
+    awful.key({ modkey,           }, "f", function (c) c.fullscreen = not c.fullscreen end),
+    awful.key({ modkey,           }, "x", function (c) c:kill() end),
+    awful.key({ modkey, "Shift"   }, "x", function () awful.util.spawn('xkill') end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
-    awful.key({ modkey,     }, "n", function (c) c.minimized = true end),
-    awful.key({ modkey,     }, "m",
+    awful.key({ modkey,           }, "n", function (c) c.minimized = true end),
+    awful.key({ modkey,           }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
             c.maximized_vertical   = not c.maximized_vertical
